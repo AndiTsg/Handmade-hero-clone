18f91d769b33f9c890faff413020a6f807d237b6
Andinet Tsegaye <andinettsegayeoo@gmail.com>
2026-06-29 23:54:42 +0300

    Add initial MessageBox test

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -6,5 +6,7 @@ WinMain(HINSTANCE Instance,
         LPSTR CommandLine,
         int ShowCode)
 {
+    MessageBoxA(0, "some text in the box.", "title",
+        MB_OK | MB_ICONINFORMATION);
     return 0;
 }
