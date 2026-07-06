540a02ee166bb9d107e2fdc64c6f30dd78f4fef3
Andinet Tsegaye <andinettsegayeoo@gmail.com>
2026-07-06 14:10:12 +0300

    no longer need compatible dc

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -1,57 +1,68 @@
 #include <windows.h>
 
-#define  local_persist static
+// no longer needed : #define  local_persist static
 #define global_variable static 
 
 //this one means only for this file or translation unit or source file
-#define  intenal static
+#define  internal static
 
 //this is glbal for now
 global_variable BOOL Running;
 
-global_variable BITMAPINFO BitmapInfo;
-global_variable void* BitMapMemory;
-global_variable HBITMAP BitmapHandle;
-global_variable HDC BitmapDeviceContext;
+global_variable BITMAPINFO BitmapInfo; //we fill out the bitmapinfo ourselves
+global_variable void* BitmapMemory;
+global_variable int BitmapHeight;
+global_variable int BitmapWidth;
+
+// following are no longer needed bcoz stretchdibit : global_variable HBITMAP BitmapHandle; global_variable HDC BitmapDeviceContext;
 
 internal void
-Win32ResizeDIBSection(int Width,int Height)   // this is for resizing our dib section that we can draw into and 
-                                              // also to create if it hasnt been before
+Win32ResizeDIBSection(int Width, int Height)   // this is for resizing our dib section that we can draw into and 
+// also to create if it hasnt been before
 {
-    BitmapInfo.bmiHeader.biSize =sizeof(BitmapInfo.bmiHeader) ;
-    BitmapInfo.bmiHeader.biWidth = Width;
-    BitmapInfo.bmiHeader.biHeight = Height;
+    if (BitmapMemory) { //to free the memory for when we want to allocate again
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE);//MEM_RELEASE frees too but if we used decommit it would leave the pages reserved(way of saying dont track but we may need the memory later)
+    }
+    BitmapHeight = Height;
+    BitmapWidth = Width;
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biHeight = -BitmapHeight; //we choose top down so we make negative
     BitmapInfo.bmiHeader.biPlanes = 1;
     BitmapInfo.bmiHeader.biBitCount = 32;
     BitmapInfo.bmiHeader.biCompression = BI_RGB;
-    /* no longer needed bc bitmap info set to static .BitmapInfo.biSizeImage = 0;  BitmapInfo.biXPelsPerMeter = 0;   BitmapInfo.biYPelsPerMeter = 0; BitmapInfo.biClrUsed = 0;BitmapInfo.biXPelsPerMeter = 0;BitmapInfo.biClrImportant=0*/
-        
-    
-    //todo bullet proof this
+    /* the following are no longer needed bc bitmap info set to static .BitmapInfo.biSizeImage = 0;  BitmapInfo.biXPelsPerMeter = 0;   BitmapInfo.biYPelsPerMeter = 0; BitmapInfo.biClrUsed = 0;BitmapInfo.biXPelsPerMeter = 0;BitmapInfo.biClrImportant=0*/
 
-    HBITMAP BitmapHandle = CreateDIBSection    //the return value HBITMAP is the handle for our created dibsection
-                            (DeviceContext, &BitMapInfo, 
-                            DIB_RGB_COLORS, &BitMapMemory ,0,0);
+    int BytesPerPixel = 4;
+    int BitmapMemorySize = (Width * Height) * BytesPerPixel;
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
 }
 
 
 internal void
-Win32UpdateWindow(HDC DeviceContext, int  X, int  Y, int  Width, int  Height)() {
-    int StretchDIBits(DeviceContext,  
-                     X,  Y, Width, Height   //   destination rectangle
-                     X, Y, Width, Height    //   source rectangle, it is the same as src since we're drawing to th ewindow wc iis destination
-                     const VOID * lpBits,
-                     const BITMAPINFO * lpbmi,
-                     DIB_RGB_COLORS, SRCCOPY);
+Win32UpdateWindow(HDC DeviceContext, RECT* WindowRect, int  X, int  Y, int  Width, int  Height) {
+
+
+    int WindowWidth = WindowRect->right - WindowRect->left;
+    int WindowHeight = WindowRect->bottom - WindowRect->top;
+
+    StretchDIBits(DeviceContext,
+        /*
+        X,  Y, Width, Height   //   destination rectangle
+        X, Y, Width, Height    //   source rectangle, it is the same as src since we're drawing to the window wc is destination
+        */
+        0, 0, BitmapWidth, BitmapHeight,
+        0, 0, WindowWidth, WindowHeight,
+        BitmapMemory,
+        &BitmapInfo,
+        DIB_RGB_COLORS, SRCCOPY);
 }
-
-
-
 
 
 WNDPROC Wndproc;
 
-LRESULT CALLBACK 
+LRESULT CALLBACK
 Win32MainWindowCallback(
     HWND Window,
     UINT Message,
@@ -63,12 +74,13 @@ Win32MainWindowCallback(
     {
     case WM_SIZE:
     {
-       RECT ClientRect; 
-       BOOL GetClientRect(window, &ClientRect){     // this is to get drawable area of a window
-           int Height = ClientRect.rcPaint.bottom - ClientRect.rcPaint.top;
-           int Width = ClientRect.rcPaint.right - ClientRect.rcPaint.left;
-           Win32ResizeDIBSection(Width, Height);
-        }
+        RECT ClientRect;
+        GetClientRect(Window, &ClientRect);   // this is to get drawable area of a window
+
+        int Height = ClientRect.bottom - ClientRect.top;
+        int Width = ClientRect.right - ClientRect.left;
+        Win32ResizeDIBSection(Width, Height);
+
         OutputDebugStringA("WM_SIZE\n");
     }break;
 
@@ -96,14 +108,17 @@ Win32MainWindowCallback(
     case WM_PAINT:
     {
         PAINTSTRUCT paint;
-        HDC DeviceContext= BeginPaint(Window,&paint );
-        int X = paint.rcPaint.left; 
+        HDC DeviceContext = BeginPaint(Window, &paint);
+        int X = paint.rcPaint.left;
         int Y = paint.rcPaint.top;
         int Height = paint.rcPaint.bottom - paint.rcPaint.top;
         int Width = paint.rcPaint.right - paint.rcPaint.left;
-        Win32UpdateWindow(Window,X, Y, Width, Height);
+
+        RECT ClientRect;
+        GetClientRect(Window, &ClientRect);
+        Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
         EndPaint(Window, &paint);
-    }break; 
+    }break;
 
     default:
     {
